47943c4ce56f8a82f6efbcaccc0d60036b088206
Jan Tattermusch <jtattermusch@google.com>
2015-10-26 12:31:22 -0700

    update generate.bat script

--- CloudPubsubExample/generate.bat
+++ CloudPubsubExample/generate.bat
@@ -1,1 +1,9 @@
-protoc -I . google\pubsub\v1\pubsub.proto google\protobuf\empty.proto google\api\annotations.proto google\api\http.proto --csharp_out=. --grpc_out=. --plugin=protoc-gen-grpc=..\packages\Grpc.Tools.0.7.0-preview20150826\tools\grpc_csharp_plugin.exe
+
+setlocal
+@rem enter this directory
+cd /d %~dp0
+
+..\packages\Google.Protobuf.3.0.0-alpha4\tools\protoc.exe -I ..\third_party\googleapis -I ..\packages\Google.Protobuf.3.0.0-alpha4\tools  ..\packages\Google.Protobuf.3.0.0-alpha4\tools\google\protobuf\empty.proto ..\third_party\googleapis\google\pubsub\v1\pubsub.proto  ..\third_party\googleapis\google\api\annotations.proto ..\third_party\googleapis\google\api\http.proto --csharp_out=. --grpc_out=. --plugin=protoc-gen-grpc=..\packages\Grpc.Tools.0.7.1\tools\grpc_csharp_plugin.exe
+exit /b %errorlevel%
+
+endlocal
